a107b8e3f79f39a5e7030e9b5a772e28284f2a92
salazarj <salazarj@seattleu.edu>
2018-06-07 15:23:50 -0400

    test 2

--- main.js
+++ main.js
@@ -1,43 +1,48 @@
-
-    function toggle(tag) {
-        var x=document.getElementsByName(tag)[0];
-        var a = x.parentNode
-        if (a.style.display=='block'){
-            a.style.display='none'
-        }else{
-            a.style.display='block'     
+document.addEventListener('DOMContentLoaded', function(){
+    let data = [
+        {
+            title: "TEST",
+            description: "Lorem ipsum dolor sit amet."
+        },
+        {
+            title: "TEST",
+            description: "Lorem ipsum dolor sit amet."
+        },
+        {
+            title: "TEST",
+            description: "Lorem ipsum dolor sit amet."
+        },
+        {
+            title: "TEST",
+            description: "Lorem ipsum dolor sit amet."
         }
+    ]
+    let demolist = document.querySelector('#demolist');
+    
+    for (let item of data) {
+        let itemTemplate = `
+            <a class="show-toggle">show me</a>
+            <div class="extra">
+                <h2>${item.title}</h2>
+                <p>${item.description}</p>
+            </div>
+        `;
+        let newListItem = document.createElement('li');
+        newListItem.innerHTML = itemTemplate;
+        demolist.appendChild(newListItem);
     }
 
-
-    function init() {
-        //this function will add show hide functionality to paired list items,
-        //as long as the answer is a list item straight after the question list item.
-        //You can also have as many separate lists as you want.
-        //all lists must be contained within a div with id QA
-
-        var obj = document.getElementById('QA');
-        var elements = obj.getElementsByTagName('li');
-               var index = 1 
-        //add javascript to question elements
-              //you could also add styling to question elements here
-        for (var i=0; i < elements.length; i+=2){
-            var element = elements[i]; 
-            element.innerHTML = "<a href='javascript:toggle(" + index + ")'>" + element.innerHTML + "</a>"
-            index = index + 1
-        } 
-        //add bookmark to answer elements and add styling
-        var index = 1
-        for (var i=1; i < elements.length; i+=2){
-            var element = elements[i]; 
-            element.innerHTML = "<a name='" + index + "' id='" + index + "'></a>" + element.innerHTML 
-            index = index + 1
-                 element.style.padding = '0px 0px 10px 20px' //add indent to answer
-                       element.style.listStyleType = 'none' //remove bullet
-            element.style.display = 'none' //hide answer element
-        }       
-     }
-
-
-window.onload = init; 
-
+    
+    let items = document.querySelectorAll('#demolist li a');
+    for (let item of items) {
+        item.addEventListener('click', function(event){
+            event.currentTarget.parentElement.classList.toggle('open');
+            if (event.currentTarget.innerHTML == 'show me') {
+                event.currentTarget.innerHTML = "hide me";
+            } else {
+                event.currentTarget.innerHTML = "show me";
+            }
+            
+        });
+    }
+});
